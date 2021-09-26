40b3a1f033794848c8a4e405253bb627f988adf0
AyanamiKiki <ayanami.kiki@gmail.com>
2021-09-26 10:27:33 +0200

    Added search engine

---
+++ src/Dictionary.js
@@ -0,0 +1,24 @@
+import "./Dictionary.css";
+import React, { useState } from "react";
+
+export default function Dictionary(){
+let [keyword, setKeyword] = useState("");
+
+function search(event){
+    event.preventDefault();
+    alert(`searching for ${keyword} definition`);
+}
+
+function handleKeywordChange(event){
+setKeyword(event.target.value);
+}
+
+    return (<div className="Dictionary">
+       <form onSubmit={search}>
+           <input 
+           type="search" 
+           autoFocus={true}
+           onChange={handleKeywordChange}/>
+       </form>
+    </div>)
+}
